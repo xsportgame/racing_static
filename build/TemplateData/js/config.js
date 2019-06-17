2027a68a4e8a2b3fd632afb42d8a70c7de593d0a
Oleg <wyndrick@gmail.com>
2019-06-17 09:53:42 +0300

    Заменил ассетбандлы

--- build/TemplateData/js/config.js
+++ build/TemplateData/js/config.js
@@ -1,8 +1,8 @@
 var config = {
-	bundles_version:26,
-	bundles_url:"https://xsportgame.github.io/racing_static/static/bundles/{0}?v=26",
-	track_info_url:"https://xsportgame.github.io/racing_static/static/tracks_data/track_{0}.bytes?v=26",
-	rs:"https://xsportgame.github.io/racing_static/static/rs.bytes?v=26", 
+	bundles_version:27,
+	bundles_url:"https://xsportgame.github.io/racing_static/static/bundles/{0}?v=27",
+	track_info_url:"https://xsportgame.github.io/racing_static/static/tracks_data/track_{0}.bytes?v=27",
+	rs:"https://xsportgame.github.io/racing_static/static/rs.bytes?v=27", 
 };
 function getConfig() {
 	 SendMessage("Main Camera", "GetConfigCallback",JSON.stringify(config));
