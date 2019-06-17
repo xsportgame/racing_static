1879a8ce7171cb8c42e861b6a0311354f0ef451f
Oleg <wyndrick@gmail.com>
2019-06-17 09:42:15 +0300

    Поправил ошибку в config.js

--- build/TemplateData/js/config.js
+++ build/TemplateData/js/config.js
@@ -1,9 +1,9 @@
 var config = {
-	bundles_version:25
-	bundles_url:"https://xsportgame.github.io/racing_static/static/bundles/{0}?v=25",
-	track_info_url:"https://xsportgame.github.io/racing_static/static/tracks_data/track_{0}.bytes?v=25",
-	rs:"https://xsportgame.github.io/racing_static/static/rs.bytes?v=25", 
-}
+	bundles_version:26,
+	bundles_url:"https://xsportgame.github.io/racing_static/static/bundles/{0}?v=26",
+	track_info_url:"https://xsportgame.github.io/racing_static/static/tracks_data/track_{0}.bytes?v=26",
+	rs:"https://xsportgame.github.io/racing_static/static/rs.bytes?v=26", 
+};
 function getConfig() {
 	 SendMessage("Main Camera", "GetConfigCallback",JSON.stringify(config));
 }
