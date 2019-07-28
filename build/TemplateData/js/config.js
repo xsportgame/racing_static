3e75a44da2192a2cc7f1097ad88e3540c5c3efbe
Oleg <wyndrick@gmail.com>
2019-07-28 12:36:29 +0300

    попытка исправить проблему с крашем во время загрузки, расставил логи

--- build/TemplateData/js/config.js
+++ build/TemplateData/js/config.js
@@ -1,9 +1,9 @@
 var config = {
-	bundles_version:27,
-	bundles_url:"https://xsportgame.github.io/racing_static/static/bundles/{0}?v=27",
-	track_info_url:"https://xsportgame.github.io/racing_static/static/tracks_data/track_{0}.bytes?v=27",
-	rs:"https://xsportgame.github.io/racing_static/static/rs.bytes?v=27", 
-};
+	bundles_version:25,
+	bundles_url:"https://xsportgame.github.io/racing_static/static/bundles/{0}?v=25",
+	track_info_url:"https://xsportgame.github.io/racing_static/static/tracks_data/track_{0}.bytes?v=25",
+	rs:"https://xsportgame.github.io/racing_static/static/rs.bytes?v=25", 
+}
 function getConfig() {
 	 SendMessage("Main Camera", "GetConfigCallback",JSON.stringify(config));
 }
